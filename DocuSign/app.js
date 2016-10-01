1292c730b03517a208dd26fc414434405880c515
Jake Sutton <jrsutton@ucsd.edu>
2016-10-01 15:19:04 -0700

    Getting data through from MongoDB

--- DocuSign/app.js
+++ DocuSign/app.js
@@ -5,21 +5,14 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/Events');
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
-
-// create mongo client outlet
-// Retrieve
-var MongoClient = require('mongodb').MongoClient;
-
-// Connect to the db
-MongoClient.connect("mongodb://localhost:27017/exampleDb", function(err, db) {
-  if(!err) {
-    console.log("We are connected");
-  }
-});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -33,8 +26,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next){
+  req.db = db;
+  next();
+});
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- DocuSign/routes/index.js
+++ DocuSign/routes/index.js
@@ -3,7 +3,18 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hackathon Manager' });
+  res.render('index', { title: 'TDocs' });
+});
+
+/* GET home page. */
+router.post('/home', function(req, res, next) {
+  var db = req.db;
+  var collection = db.get('Events');
+  collection.find({},{},function(e,docs){
+    res.render('home', {
+      eventlist : docs
+    });
+  });
 });
 
 module.exports = router;
